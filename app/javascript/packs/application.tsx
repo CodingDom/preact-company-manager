a68b9dd0c8e7ecd84b08ad6a993b8329e656207c
Dom <44685635+CodingDom@users.noreply.github.com>
2025-04-23 16:48:46 -0400

    chore: removed console log

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -1,21 +1,19 @@
 // Entry point for the build script in your package.json
 
 /** @jsx h */
-import { h, render } from 'preact';
-import { loadPage } from '../utils/loadPage';
+import { h, render } from "preact";
+import { loadPage } from "../utils/loadPage";
 
-console.log('Started application')
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const root = document.getElementById('preact-root');
+document.addEventListener("DOMContentLoaded", async () => {
+  const root = document.getElementById("preact-root");
   if (!root) return;
 
   const pageName = root.dataset.component!;
-  const props = JSON.parse(root.dataset.props || '{}');
+  const props = JSON.parse(root.dataset.props || "{}");
 
   const Page = await loadPage(pageName);
-  
+
   if (Page) {
-    render(<Page {...props}/>, root);
+    render(<Page {...props} />, root);
   }
 });
