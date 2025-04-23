c53287ed1146619125df60de7312da1e22ec45d3
Dom <44685635+CodingDom@users.noreply.github.com>
2025-04-23 07:45:43 -0400

    build: set up sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,61 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+
   devtool: "source-map",
+
   entry: {
-    application: "./app/javascript/packs/application.tsx"
+    application: "./app/javascript/packs/application.tsx",
   },
+
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
     alias: {
-      react: 'preact/compat',
-      'react-dom': 'preact/compat',
+      react: "preact/compat",
+      "react-dom": "preact/compat",
     },
   },
+
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
+      maxChunks: 1,
+    }),
   ],
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      // Handle regular .scss files (non-modules)
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                loadPaths: [
+                  path.resolve(__dirname, "app/assets/stylesheets/globals"),
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
-}
+};
