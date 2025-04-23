50a038c560d27b7da02356abb0d496d296e9e6fe
Dom <44685635+CodingDom@users.noreply.github.com>
2025-04-23 16:44:45 -0400

    feat: set up Button component story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,54 @@
-import type { StorybookConfig } from '@storybook/preact-webpack5';
+import type { StorybookConfig } from "@storybook/preact-webpack5";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/preact-webpack5",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/preact-webpack5",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    // Enable CSS Modules for .module.scss
+    config.module?.rules?.push({
+      test: /\.module\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "sass-loader",
+          options: {
+            sassOptions: {
+              loadPaths: [
+                path.resolve(__dirname, "../app/assets/stylesheets/globals"),
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      };
+    }
+
+    return config;
+  },
 };
 export default config;
