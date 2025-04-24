dac5bdbadb3335ec8d2f97ce0cb4bebad1198cb4
Dom <44685635+CodingDom@users.noreply.github.com>
2025-04-24 07:16:30 -0400

    docs: updated Button story to allow for button type radios

--- stories/Button.stories.jsx
+++ stories/Button.stories.jsx
@@ -1,23 +1,24 @@
 import { fn } from "@storybook/test";
 
-import { Button } from "../app/javascript/components/atoms/Button";
+import { Button, ButtonTypes } from "../app/javascript/components/atoms/Button";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-  title: "Example/Button",
+  title: "Library/Atoms/Button",
   component: Button,
   tags: ["autodocs"],
   argTypes: {
+    type: {
+      options: ButtonTypes,
+      control: { type: "radio" },
+    },
     onClick: { action: "onClick" },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
+    type: ButtonTypes.Primary,
     label: "Button",
   },
 };
